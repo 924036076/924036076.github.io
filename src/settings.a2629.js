63b43cbcbcce820ba835ef0da76123ae335ffc62
HeiSangYang <924036076@qq.com>
2021-10-06 15:09:31 +0800

    构建
    网站测试

---
+++ src/settings.a2629.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scene/game2048.fire",orientation:"portrait",jsList:[],bundleVers:{internal:"01588",main:"9d148"}};
